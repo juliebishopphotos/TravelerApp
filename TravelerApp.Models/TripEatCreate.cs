d91150e701be169f177be8e2ad7939c9549e439d
julie <juliebishopphotos@gmail.com>
2021-08-25 19:32:50 -0400

    added styling elements

--- TravelerApp.Models/TripEatCreate.cs
+++ TravelerApp.Models/TripEatCreate.cs
@@ -4,14 +4,15 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelerApp.Data;
 
 namespace TravelerApp.Models
 {
     public class TripEatCreate
     {
-        [Display(Name="Trip Id")]
-        public int TripId { get; set; }
+        [Display(Name = "Trip Id")]
+        public int TripId { get; set; } 
         [Display(Name ="Restaurant Id")]
-        public int EatId { get; set; }
+        public int EatId { get; set; } 
     }
 }
